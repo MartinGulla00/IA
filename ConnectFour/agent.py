9baae48cc72a3bb9633688fb7345a3718850dd1d
sbarretoKM <sofia.barreto@kinamic.com>
2023-07-03 23:17:15 -0300

    agrego Q learn

--- ConnectFour/agent.py
+++ ConnectFour/agent.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from board import Board
+import numpy as np
 
 class Agent(ABC):
 
@@ -11,21 +12,104 @@ class Agent(ABC):
     def heuristic_utility(self, board: Board):
         return NotImplementedError
 
+from abc import ABC, abstractmethod
+from board import Board
+import numpy as np
 
+class Agent(ABC):
 
-class InputAgent(Agent):
+    @abstractmethod
+    def next_action(self, obs):
+        pass
+    
+    @abstractmethod
+    def heuristic_utility(self, board: Board):
+        pass
+
+from collections import defaultdict
+from collections import defaultdict
+
+class QLearningAgent(Agent):
+    def __init__(self, board_shape, player, learning_rate=0.1, discount_factor=0.9, initial_epsilon=0.9, epsilon_decay=0.999):
+        self.board_shape = board_shape
+        self.player = player
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.q_table = defaultdict(lambda: np.zeros(board_shape[1]))
+
+    def encode_state(self, board):
+        state = ""
+        for row in board._grid:
+            state += "".join(map(str, row))
+        return state
 
     def next_action(self, obs):
-        while True:
-            try:
-                input_value = int(input())
-                return input_value
-            except ValueError:
-                print("Please insert a number.")
+        state = self.encode_state(obs)
+        if np.random.uniform(0, 1) < self.epsilon:
+            return np.random.choice(obs.get_posible_actions())
+        else:
+            q_values = {action: self.q_table[state][action] for action in obs.get_posible_actions()}
+            return max(q_values, key=q_values.get)
+
+    def update_q_table(self, state, action, next_state, reward, done):
+        current_q = self.q_table[state][action]
+        max_next_q = np.max(self.q_table[next_state]) if not done else 0
+        new_q = current_q + self.learning_rate * (reward + self.discount_factor * max_next_q - current_q)
+        self.q_table[state][action] = new_q
+        self.epsilon *= self.epsilon_decay  # decay epsilon
+
+    def train(self, num_episodes, verbose=False):
+        for episode in range(num_episodes):
+            obs = Board(self.board_shape[0], self.board_shape[1])  # Create a new board for each episode
+            done = False
+            while not done:
+                current_state = self.encode_state(obs)
+                action = self.next_action(obs)
+                reward, done = self.take_action(obs, action)
+                next_state = self.encode_state(obs)
+                self.update_q_table(current_state, action, next_state, reward, done)
+                if verbose:
+                    print("Episode:", episode, "Action:", action, "Reward:", reward)
+                    obs.render()
+
+    def take_action(self, obs, action):
+        success = obs.add_tile(action, self.player)  # Use the add_tile method instead of step
+        done = obs.is_final()
+        if done:
+            if obs.winner == self.player:
+                return 2, True
+            elif obs.is_full():
+                return 0, True
+            else:
+                return self.heuristic_utility(obs), False
+        elif not success:  # Reward of -1 if the action is invalid
+            return -1, False  
+        else:
+            return 0, False  # No immediate reward for valid moves
 
     def heuristic_utility(self, board: Board):
-        return 0
-    
+        utility = 0
+
+        opponent = 2 if self.player == 1 else 1
+
+        for row in board._grid:
+            for i in range(board._length - 3):
+                if (row[i] == row[i+3] == self.player) and (row[i+1] == row[i+2] == opponent):
+                    utility += 1
+                elif (row[i] == row[i+3] == opponent) and (row[i+1] == row[i+2] == self.player):
+                    utility -= 1
+
+        for i in range(board._heigth - 3):
+            for j in range(board._length - 3):
+                if (board._grid[i][j] == board._grid[i+3][j+3] == self.player) and (board._grid[i+1][j+1] == board._grid[i+2][j+2] == opponent):
+                    utility += 1
+                elif (board._grid[i][j] == board._grid[i+3][j+3] == opponent) and (board._grid[i+1][j+1] == board._grid[i+2][j+2] == self.player):
+                    utility -= 1
+
+        return utility
+
 
 class ExpectimaxAgent(Agent):
     def __init__(self, player, max_depth=3):
@@ -153,3 +237,17 @@ class ExpectimaxAgent(Agent):
 
     def in_board(self, board: Board, x: int, y: int):
         return 0 <= x < board.heigth and 0 <= y < board.length
+
+
+class InputAgent(Agent):
+
+    def next_action(self, obs):
+        while True:
+            try:
+                input_value = int(input())
+                return input_value
+            except ValueError:
+                print("Please insert a number.")
+
+    def heuristic_utility(self, board: Board):
+        return 0
